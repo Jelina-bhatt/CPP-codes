25005974f22f3b6db656e80815c1331e4034faf0
jelina bhatt <jelinabhatt1@gmail.com>
2024-06-16 23:35:08 +0545

    init commit

--- unit_1/max'm.cpp
+++ unit_1/max'm.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int max(int, int, int);
 
 int main(){
-    int x, y, z,c;
+    int x, y, z;
     cout << "Enter three numbers: ";
     cin >> x >> y >> z;
-    c=max(x, y, z);
-    cout << "Max = " << c << endl;
+    int result = max(x, y, z);
+    cout << "Max = " << result << endl;
     return 0;
 }
 
 int max(int a, int b, int c) {
-    if (a >b && a > c) {
+    if (a >= b && a >= c) {
         return a;
-    } else if (b > a && b >c) {
+    } else if (b >= a && b >= c) {
         return b;
     } else {
         return c;
