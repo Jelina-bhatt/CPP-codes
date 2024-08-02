aa9807b7f7e32c24a171e68c097a18ed9ad80d5f
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-08-02 23:06:58 +0545

    Create Return_value.cpp

---
+++ function/Return_value.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Function declaration
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    int result = multiply(4, 5);
+    cout << "Product: " << result << endl;
+    return 0;
+}
