e23b02c03baecb54198d59b33b0511516fc99ff7
jelina bhatt <jelinabhatt1@gmail.com>
2024-09-27 20:03:52 +0545

    unary prefix codes committed

---
+++ operator overloading/unary_operator-preefix.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class counter
+{
+    int count;
+    public: counter()
+    {
+        count = 0;
+    }
+    void operator ++();
+    void display()
+    {
+        cout<<"Count = "<<count<< endl;
+    }
+};
+void counter:: operator ++()
+{
+    count ++;
+}
+int main()
+{
+    counter c1,c2;
+    cout<<"Initial value of c1= "<<endl;
+    c1.display();
+     cout<<"Initial value of c2= "<<endl;
+     c2.display();
+    ++c1;
+    ++c2;
+    ++c2;
+      ++c2;
+    ++c2;
+    cout<<"After Increment c1 = "<<endl;
+    c1.display();
+    cout<<"After Increment c2 = "<<endl;
+    c2.display();
+    return 0;
+}
