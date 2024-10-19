ab48768d509d67b5702e1bd3865c5119faa5d0ff
jelina bhatt <jelinabhatt1@gmail.com>
2024-10-19 22:12:59 +0545

    friend class

---
+++ function/friendclass.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class B;
+class A
+{
+private:
+    int x,y;
+public:
+  void enter(){
+    cout<<"enter the value of x and y";
+    cin>>x>>y;
+  }
+  friend class B;
+};
+class B{
+    public:void display(A k){
+        cout<<"x="<<k.x<<endl;
+        cout<<"y="<<k.y<<endl;
+    }
+};
+ int main(){
+    A obj1;
+    B obj2;
+    obj1.enter();
+    obj2.display(obj1);
+    return 0;
+
+ }
