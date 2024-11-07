06d222b10e2e270795f25c77f902e8632394ef03
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-11-07 19:12:36 +0545

    Update Maxm_num.cpp

--- unit_1/Maxm_num.cpp
+++ unit_1/Maxm_num.cpp
@@ -5,7 +5,6 @@ int main() {
     int num1, num2;
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
-
     if (num1 > num2) {
         cout << num1 << " is greater than " << num2 << endl;
     } else if (num2 > num1) {
